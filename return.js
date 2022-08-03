660d80628dd40865a512e7b1277987df39c639a8
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-03 23:20:56 +0600

    function return modify

--- return.js
+++ return.js
@@ -1,3 +1,5 @@
+//This function is about Marco filling station total sales money and how much litre sold in a day.
+
 function totalSales(aShift, bShift) {
     console.log('Day Shift: ', aShift, 'Night Shift: ', bShift);
     let shiftTotals = aShift + bShift;
@@ -10,3 +12,6 @@ let perShiftMoneyA = 32154;
 let perShiftMoneyb = 29541;
 let totalLitresSales = totalSales(perShiftMoneyA, perShiftMoneyb);
 console.log('Todays total sale - ', totalLitresSales.toFixed(2), ' Litter');
+
+//This is for SR Enterprise to maintain cylinder stock
+
