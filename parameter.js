e8115dd100b0631bdb2f8fb3e2beb978bfe54fc3
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-03 22:02:56 +0600

    variable as parameter value

--- parameter.js
+++ parameter.js
@@ -1,6 +1,10 @@
 // Declear a function name with parameter name money
 function bringSingara(money) {
-    console.log('Mama singara den 10 taka nen');
-    console.log(money);
+    console.log(money, 'Taka paici.');
+    console.log('Mama, Ei nen singara');
 }
-bringSingara(10); // parameter value is 10.
+// bringSingara(10); // parameter value is 10.
+
+//we can use variable as a parameter value 
+let taka = 210;
+bringSingara(taka);
