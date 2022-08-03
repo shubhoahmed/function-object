a67653ede5253dbe46e4a9a479f2a41bfeca986a
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-03 23:12:45 +0600

    return function used

---
+++ return.js
@@ -0,0 +1,12 @@
+function totalSales(aShift, bShift) {
+    console.log('Day Shift: ', aShift, 'Night Shift: ', bShift);
+    let shiftTotals = aShift + bShift;
+    console.log('Total Money: ', shiftTotals);
+    let rate = 57;
+    let totalLitres = shiftTotals / rate;
+    return totalLitres;
+}
+let perShiftMoneyA = 32154;
+let perShiftMoneyb = 29541;
+let totalLitresSales = totalSales(perShiftMoneyA, perShiftMoneyb);
+console.log('Todays total sale - ', totalLitresSales.toFixed(2), ' Litter');
